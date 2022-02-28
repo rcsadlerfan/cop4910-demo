b6eef550534c0d192ad9cd5961fe307abf3ddb46
Ryan Carnovsky <rcarnovsky@gmail.com>
2022-02-28 12:42:58 -0500

    Test changes

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col w-2/3 h-screen justify-center m-auto text-center space-y-24">
     <h1 class="text-3xl font-semibold">CI/CD Pipelines Demo</h1>
     <div class="flex justify-center">
-      <div class="flex justify-center items-center w-16 h-16 border border-gray-500 shadow-lg rounded-full animate-wiggle">
+      <div class="flex justify-center items-center w-16 h-16 border border-gray-500 shadow-lg rounded-full animate-bounce">
         <p class="text-5xl">&darr;</p>
       </div>
     </div>
